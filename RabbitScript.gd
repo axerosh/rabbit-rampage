c49f9d390dc767bf32dc73e9c369dfc3878a6a92
Axel Nordanskog <axerosh@gmail.com>
2022-01-28 21:16:43 +0100

    Add farmers dropping collectable carrots and flesh

--- RabbitScript.gd
+++ RabbitScript.gd
@@ -1,14 +1,17 @@
 extends KinematicBody2D
 
-var moveSpeed: int = 200
+var moveSpeed: int = 10000
+
+var carrots_collected: int = 0
+var flesh_collected: int = 0
 
 var velocity: Vector2 = Vector2()
 var facingDir: Vector2 = Vector2(0, 1)
 
-func _ready():
+func _ready() -> void:
 	pass # Replace with function body.
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	
 	velocity = Vector2()
 	
@@ -27,4 +30,14 @@ func _physics_process(delta):
 		
 	velocity = velocity.normalized()
 		
-	move_and_slide(velocity * moveSpeed)
+	var _movement: Vector2 = move_and_slide(velocity * moveSpeed * delta)
+
+func _on_CollectionArea2D_area_entered(area: Area2D) -> void:
+	if (area is Carrot):
+		carrots_collected += 1
+		print("Carrots collected: " + str(carrots_collected))
+		area.queue_free()
+	if (area is Flesh):
+		flesh_collected += 1
+		print("Flesh collected: " + str(flesh_collected))
+		area.queue_free()
---
+++ humans/farmer.gd
@@ -0,0 +1,36 @@
+extends KinematicBody2D
+class_name Farmer
+
+const flesh_scene: PackedScene = preload("res://collectables/flesh.tscn");
+const carrot_scene: PackedScene = preload("res://collectables/carrot.tscn");
+
+const time_between_drops: float = 2.0;
+const drops_before_death: int = 2;
+const speed: Vector2 = Vector2(4000, 0);
+
+var time_until_drop: float = time_between_drops;
+var carrots_dropped: int = 0;
+
+func _physics_process(delta: float) -> void:
+	var _movement: Vector2 = move_and_slide(speed * delta)
+
+func _process(delta: float) -> void:
+	time_until_drop -= delta;
+	if (time_until_drop <= 0.0):
+		time_until_drop = time_between_drops;
+		if (carrots_dropped < drops_before_death):
+			carrots_dropped += 1;
+			drop_carrot()
+		else:
+			drop_dead()
+
+func drop_carrot() -> void:
+	var carrot: Carrot = carrot_scene.instance();
+	get_tree().get_root().add_child(carrot);
+	carrot.global_position = self.global_position;
+
+func drop_dead() -> void:
+	var flesh: Flesh = flesh_scene.instance();
+	get_tree().get_root().add_child(flesh);
+	flesh.global_position = self.global_position;
+	queue_free();
