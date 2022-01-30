501f958612b5fc33159020743df24f093480bb10
Kevin L.A <somjai.kevin@gmail.com>
2022-01-30 11:46:28 +0100

    Add attempt at fading music

---
+++ BgmPlayer.gd
@@ -0,0 +1,21 @@
+extends Node2D
+class_name BgmPlayer
+
+func _ready():
+	if DayNightManager.is_night:
+		$"Bgm Day".volume_db = -80
+		$"Bgm Night".volume_db = 0
+	else:
+		$"Bgm Day".volume_db = 0
+		$"Bgm Night".volume_db = -80
+		
+	DayNightManager.connect("day_night_changed", self, "_day_night_changed")
+
+func _exit_tree():
+	DayNightManager.disconnect("day_night_changed", self, "_day_night_changed")
+
+func _day_night_changed(is_night: bool):
+	if is_night:
+		$AnimationPlayer.play("FadeToNight")
+	else:
+		$AnimationPlayer.play("FadeToDay")
